6a7ca0ce20d9bc2c81a680a6c0d4673812de9546
Tano <tano.design@gmail.com>
2013-01-26 17:52:47 +0100

    fix

--- scripts.js
+++ scripts.js
@@ -1309,7 +1309,7 @@ var commands = {
         "/tier [name]: To view the tier(s) of a person.",
         "/battlehistory [name]: To view a person's battle history.",
         "/channelusers [channel]: Lists users on a channel.",
-		"/em [emoticon]: Submits emoticons."
+		"/em [emoticon]: Submits emoticons.",
 		"/htmlmessage: Allows little use of HTML."
     ],
     admin: // CHANGE
@@ -1331,7 +1331,7 @@ var commands = {
 		"/updateann: Updates announcement the web.",
 		"/updateplugin [plugin]: Updates plugin from the web.",
         "/updateScripts: Updates scripts from the web.",
-		"/html: Allows use of HTML."
+		"/html: Allows use of HTML.",
 		"/font: BIG text."
     ],
     owner: // CHANGE
@@ -1359,7 +1359,7 @@ var commands = {
         "/updatetierchecks: To update tier checks.",
         "/togglerainbow: [on/off]: To turn rainbow on or off.",
         "/towner[s] [name]: makes someone a tournament owner (tours.js plugin needs to be installed for this to work)"
-    ]
+    ],
 	megauser: // CHANGE
     [
 				"/tour [tier]:[number]:[type]: Starts a tournament in set tier for the selected number of players. Type is optional and can be set to Singles, Doubles or Triples.",
@@ -1371,7 +1371,7 @@ var commands = {
 				"/sub name1:name2: Replaces name1 with name2 in the tournament.",
 				"/cancelBattle name1: Allows the user or their opponent to forfeit without leaving the tournament their current battle so they can battle again with correct clauses.",
 				"/rmqueue [tier]: Removes a specified tier from the tournament queue."
-	];
+	]
 };
 
 /* Start script-object
