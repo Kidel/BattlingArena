df4a0eab370afdd64ebd790c1b24064ab10da51d
Tano <tano.design@gmail.com>
2013-01-28 20:13:49 +0100

    added eventlist command

--- scripts.js
+++ scripts.js
@@ -4806,6 +4806,25 @@ ownerCommand: function(src, command, commandData, tar) {
 		sys.sendMessage(src, "Tier: " + tier);
 		return;
 	}
+	if (command == "eventlist") {
+		var a = events();
+		for (var x = 0; x < a.length; x++) {
+			var roba = a[x].split('%%');
+			var nome = roba[0];
+			var data = roba[1];
+			var link = roba[2];
+			var tier = roba[3];
+			sys.sendMessage(src, "Event " + (x+1));
+			sys.sendMessage(src, "Nome: " + nome);
+			sys.sendMessage(src, "Data: " + data);
+			sys.sendMessage(src, "Link: " + link);
+			sys.sendMessage(src, "Tier: " + tier);
+			sys.sendMessage(src, "");
+		}
+		return;
+	}
+	
+	
     if (command == "changeauth" || command == "changeauths") {
         var pos = commandData.indexOf(' ');
         if (pos == -1) return;
